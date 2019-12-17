697f1ccc9f4ae989d204fa2a327b8b2eabfdde6f
dvirbenshlush <dvirbsh95@gmail.com>
2019-12-17 20:54:52 +0200

    created Home component

---
+++ client/memory-game/src/components/Home.component.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+
+
+
+
+export default class Home extends Component{
+
+    render(){
+        return(
+<div className="App">
+<header className="App-header">
+  <h1 className="app-title">Memory Game</h1>
+</header>
+<div className="btn" id="login_btn">Login</div>
+<div className="btn" id="signup_btn">Signup</div>
+</div>
+         ) }
+}
