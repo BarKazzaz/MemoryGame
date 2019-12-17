4762c86e065e8c841fa2ddf01c9afc7458b20a8f
dvirbenshlush <dvirbsh95@gmail.com>
2019-12-17 19:09:21 +0200

    initial commit

---
+++ client/memory-game/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="app-title">Memory Game</h1>
+      </header>
+      <div className="btn" id="login_btn">Login</div>
+      <div className="btn" id="signup_btn">Signup</div>
+    </div>
+  );
+}
+
+export default App;
