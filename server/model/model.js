f3e75d1600ccc5ebf9444efcc7bf90d8a5c865bf
bar.kazzaz <barkazzaz@gmail.com>
2020-03-09 00:37:00 +0200

    +mongoDB with find+update functions

---
+++ server/model/model.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://bar123:bar123@memorygamecluster-rae0j.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+function printBar(){
+    client.connect(err => {
+        const collection = client.db("Legends-Memory-Game").collection("Users");
+        // perform actions on the collection object
+        collection.find({"name" : "Bar"}).toArray((err, res)=>{
+            if(err) console.error(err);
+            else console.log(res);
+        });
+        client.close();
+    });
+}
+function update(query, newValues, callback){
+    client.connect(err => {
+        const collection = client.db("Legends-Memory-Game").collection("Users");
+        // perform actions on the collection object
+        // collection.updateOne(query, newValues, (err, res) => {
+        _query = {name:"Bar"};
+        _newValues = {$set : {name:"Kazzaz", score:10} };
+        collection.updateOne(query, newValues, (err, res) => {
+            // Promise.resolve(callback(err, res))
+            // .then(client.close());
+            logger = (_err, _res) => {
+                if(err) console.log(err);
+                console.log(res);
+            }
+            Promise.resolve(callback(err,res))
+            .then(client.close());
+        });
+    });
+}
+
+module.exports = { printBar : printBar, update : update };
