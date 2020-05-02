7d16c4c7989deb3ea83f97c9ca44005077600771
bar.kazzaz <bar.kazzaz@imperva.com>
2020-05-02 10:07:56 +0300

    use userID from DB add to rudeMessages

--- server/ServerHelpers.js
+++ server/ServerHelpers.js
@@ -2,9 +2,11 @@
 const path = require("path");
 const Room = require(path.join(__dirname, "Room"));
 const Player = require(path.join(__dirname, "Player"));
-const ClassifierPromise =  require(path.join(__dirname, "MessageClassifier"));
+const ClassifierPromise = require(path.join(__dirname, "MessageClassifier"));
+const Model = require(path.join(__dirname, "model/model.js"));
+
 let MessageClassifier;
-ClassifierPromise().then((cls)=>{
+ClassifierPromise().then((cls) => {
     MessageClassifier = cls;
 })
 
@@ -14,53 +16,53 @@ const rowSize = 3;
 const colSize = 4;
 let roomsList = []; //rooms list
 
-function getRandomName(){
+function getRandomName() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-function generateCardsBoard(){
+function generateCardsBoard() {
     let matrix = [];
-    for(let i=1;i<7;i++){
+    for (let i = 1; i < 7; i++) {
         matrix.push(i);
         matrix.push(i);
     }
-    matrix.sort((a,b)=>0.5-Math.random());
-    let arr= [];
-    for(let i=0;i<rowSize;i++){
+    matrix.sort((a, b) => 0.5 - Math.random());
+    let arr = [];
+    for (let i = 0; i < rowSize; i++) {
         arr[i] = [];
-        for(let j=0;j<colSize;j++){
+        for (let j = 0; j < colSize; j++) {
             arr[i].push(matrix.pop());
         }
     }
     return arr;
 }
 const NAMES = ["Bar", "Kazzaz", "Kazza", "Baruch", "Barboor", "Kazu"];
-function setListeners(socketIo, socket){
+function setListeners(socketIo, socket) {
     socket.on("quickPlay", (data) => {
         console.log("your name is:", data);
         let player = new Player();
         let foundRoom = false;
-        player.id = getRandomName();
+        player.id = data.id;
         player.name = data.name;
-        for(let room of roomsList) {
+        for (let room of roomsList) {
             if (room.players.length < maxPlayers) {
                 room.players.push(player);
                 socket.join(room.name);
                 foundRoom = true;
                 console.log(room.name);
-                socket.emit("didJoin", { room: room.name, board: room.board, id: player.id});
-                if(room.players.length === maxPlayers)
+                socket.emit("didJoin", { room: room.name, board: room.board, id: player.id });
+                if (room.players.length === maxPlayers)
                     socketIo.sockets.in(room.name).emit("lets start", { startingPlayer: room.players[0].id, players: room.players });
             }
         }
-        if(!foundRoom){
+        if (!foundRoom) {
             let name = getRandomName();
             let room = new Room(name, maxPlayers);
             room.players.push(player);
             room.board = generateCardsBoard();
             roomsList.push(room);
             socket.join(room.name);
-            socket.emit("didJoin", { room: room.name, board: room.board, id: player.id});
+            socket.emit("didJoin", { room: room.name, board: room.board, id: player.id });
         }
     });
     socket.on("switchTurns", (data) => {
@@ -71,33 +73,38 @@ function setListeners(socketIo, socket){
     });
     socket.on("chatMsg", (data) => {
         //TODO: check for bad words
-        // let msgClass = MessageClassifier.classify(data.message.content);
-        let msgClass = MessageClassifier.getClassifications(data.message.content);
+        let msgClass = MessageClassifier.classify(data.message.content);
+        // let msgClassication = MessageClassifier.getClassifications(data.message.content);
         console.log(msgClass);
+        if (msgClass === 'negative')
+            Model.addToRudeMessages(data.message.from, data.message.content)
+                .then(data => {
+                    console.log(data);
+                }).catch(err => console.log(err))
         socketIo.sockets.in(data.room).emit("chatMsg", data.message);
     });
-    socket.on("leaver", (data)=>{
+    socket.on("leaver", (data) => {
         //data = {room : id, player : id}
         //TODO: DB should be updated
         const room = getRoom(data.room);
-        if(!room) return;
+        if (!room) return;
         room.removePlayer(data.player);
-        if(room.players.length > 0)
+        if (room.players.length > 0)
             socketIo.sockets.in(data.room).emit("leaver");
         else
             deleteRoom(room.name);
     });
 }
-function deleteRoom(name){
+function deleteRoom(name) {
     console.log("deleting room:", name);
     roomsList = roomsList.filter(r => r.name != name);
     console.log(roomsList);
 }
 
-function getRoom(name){
+function getRoom(name) {
     return roomsList.find(r => r.name === name)
 }
 
 module.exports = {
-    setListeners : setListeners
+    setListeners: setListeners
 }
