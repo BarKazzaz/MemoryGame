9c3916809dfb3f1d573325e69da2a829d6c40e5a
bar.kazzaz <bar.kazzaz@imperva.com>
2020-05-02 10:03:05 +0300

    sending id to server

--- client/memory-game/src/components/Game.component.js
+++ client/memory-game/src/components/Game.component.js
@@ -38,7 +38,7 @@ export default class Game extends Component {
     componentDidMount() {
         let usr = JSON.parse(localStorage.getItem('user'));
         if (usr)
-            this.state.socket.emit("quickPlay", { name: usr.name });
+            this.state.socket.emit("quickPlay", { id: usr._id, name: usr.name });
         this.state.socket.on("didJoin", (data) => {
             let cards = data.board;
             let cardElms = [];
