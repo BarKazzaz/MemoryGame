51803a0542afb4e9a09dbb3320089d26a3b5df41
bar.kazzaz <bar.kazzaz@imperva.com>
2020-05-02 10:02:02 +0300

    allowing direct access to /about

--- client/memory-game/src/App.js
+++ client/memory-game/src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp.component'
-import Login from './components/Login.component'
+import About from './components/About.component'
 import Home from './components/Home.component'
-import Map from "./components/Map.component";
-import Admin from "./components/Admin.component";
 
 
 function App() {
@@ -18,8 +16,10 @@ function App() {
           <Route exact path="/signup">
             <SignUp/>
           </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
           <Route path="*">
-            {/* TODO: make this a "default" component */}
             <div id="defaultRouteDiv">
               <h1>404 - Page not found</h1>
             </div>
