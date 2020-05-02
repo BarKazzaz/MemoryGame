5d434963eb9c95e60acbe3284a04f8e12e576c5c
bar.kazzaz <bar.kazzaz@imperva.com>
2020-05-02 04:45:14 +0300

    map shows real users coordinates...too real -_-'

--- client/memory-game/src/components/Map.component.js
+++ client/memory-game/src/components/Map.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './styles/marker.css'
 
+const SERVER_ADDRESS = process.env.NODE_ENV === "development" ? 'http://localhost:5000' : "";
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Marker = (props) => {
@@ -15,6 +16,14 @@ const Marker = (props) => {
 };
 
 class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: [],
+            status: 'pending'
+        }
+        this.renderMarkers = this.renderMarkers.bind(this);
+    }
     static defaultProps = {
         center: {
             lat: 31.968910,
@@ -23,39 +32,51 @@ class Map extends Component {
         zoom: 2
     };
 
-    componentDidMount(){
+    componentDidMount() {
         // TODO: request all countries lat and lng
+        fetch(`${SERVER_ADDRESS}/api/getAllCountries`)
+            .then(res => res.json())
+            .then(data => { console.log(data); return data })
+            .then(data => this.setState({ status: 'ready', countries: data.content }))
+            .then(() => { console.log(this.state.countries) })
+            .catch(err => { console.log(err) });
+    }
+
+    renderMarkers() {
+        return this.state.countries.map((e, i) => {
+            return <Marker
+                color='blue'
+                style={this.markerStyle}
+                lat={e.coordinates.lat}
+                lng={e.coordinates.lng}
+                text={e.country}
+            />
+        })
     }
 
     render() {
-        return (
+        if (this.state.status === 'ready')
+            return (
+                // Important! Always set the container height explicitly
+                <div style={{ marginLeft: '25%', height: '50%', width: '50%' }}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyBfOzLwfaP6BEPAMGeVS5oR_ayizWAHn4Q' }}
+                        defaultCenter={this.props.center}
+                        center={this.props.center}
+                        defaultZoom={this.props.zoom}
+                    >
+                        {this.renderMarkers()}
+                    </GoogleMapReact>
+                </div>
 
-            // Important! Always set the container height explicitly
-            <div style={{ marginLeft: '25%', height: '50%', width: '50%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyBfOzLwfaP6BEPAMGeVS5oR_ayizWAHn4Q' }}
-                    defaultCenter={this.props.center}
-                    center={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <Marker
-                        color='blue'
-                        style={this.markerStyle}
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
-                    />
-                    <Marker
-                        color='blue'
-                        style={this.markerStyle}
-                        lat={0.0}
-                        lng={30.337844}
-                        text="Another Marker"
-                    />
-                </GoogleMapReact>
-            </div>
-
-        );
+            );
+        else if (this.state.status === 'pending'){
+            return(
+                <div style={{ backgroundColor: 'grey', marginLeft: '25%', height: '50%', width: '50%' }}>
+                    <p>Loading...</p>
+                </div>
+            )
+        }
     }
 }
 
